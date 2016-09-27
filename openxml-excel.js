b38b8443615a46e9b86d999f23822df2a0f9c6e8
xkonishi <konishi.cnb@gmail.com>
2016-09-27 11:34:29 +0900

    a

---
+++ openxml-excel.js
@@ -0,0 +1,19 @@
+﻿(function(){
+	//スプレッドシート要素名
+    const S = openXml.S;
+    //名前空間なしの要素名
+    const NN = openXml.NoNamespace;
+
+    //属性
+    const XAttribute = Ltxml.XAttribute;
+    //エレメント
+    const XElement = Ltxml.XElement;
+	
+	let pkg;
+
+    /************************ openXml.Excel **************************/
+    openXml.Excel = function(officedoc) {
+		pkg = new openXml.OpenXmlPackage(officedoc);
+	};
+	
+}());
