19309f4b640d16eb3f1161500bff6a1fd9407461
xkonishi <konishi.cnb@gmail.com>
2016-09-27 17:09:06 +0900

    x

--- openxml-excel.js
+++ openxml-excel.js
@@ -8,12 +8,22 @@
     const XAttribute = Ltxml.XAttribute;
     //エレメント
     const XElement = Ltxml.XElement;
-	
+
 	let pkg;
 
     /************************ openXml.Excel **************************/
     openXml.Excel = function(officedoc) {
 		pkg = new openXml.OpenXmlPackage(officedoc);
 	};
-	
+
+    openXml.Excel.prototype.workbook = function() {
+        obj = {};
+
+        let workbookPart = pkg.workbookPart();
+        let wbXDoc = workbookPart.getXDocument();
+        let sheets = wbXDoc.root.element(S.sheets).elements(S.sheet);
+
+        return obj;
+    }
+
 }());
