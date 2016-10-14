50a844c26c784c6bef91518586d74a569046f29b
xkonishi <konishi.cnb@gmail.com>
2016-10-14 17:06:37 +0900

    x

--- openxml-word.js
+++ openxml-word.js
@@ -4,7 +4,6 @@
     let pkg;
     let mnPart;
     let mnXDoc;
-    let mergeFields;
 
     openXml.Word = function(officedoc) {
         pkg = new openXml.OpenXmlPackage(officedoc);
@@ -13,7 +12,52 @@
     };
 
     openXml.Word.prototype.merge = function(mergedata) {
-        mergeFields = getMergeFields();
+        let mergeFields = getMergeFields();
+
+        let data = mergedata[0];
+        Object.keys(data).forEach(function(key, index, ar) {
+            if ((typeof data[key]) !== 'object') {
+                let val = data[key];
+                if (mergeFields[key]) {
+                    let el = mergeFields[key].el;
+                    if (el) {
+                        el.element(openXml.W.fldSimple).remove();
+                        el.add(new Ltxml.XElement(openXml.W.r, new Ltxml.XElement(openXml.W.t, val)));
+                    }
+                }
+            }
+            else {
+                let sfdc_childobj = key;
+                if (mergeFields[sfdc_childobj]) {
+                    let obj = data[sfdc_childobj];
+
+                    //2行目以降を追加
+                    let tr = mergeFields[sfdc_childobj].el;
+                    for (let i=1; i<obj.records.length; i++) {
+                        tr.parent.add(new Ltxml.XElement(tr));
+                    }
+
+                    for (let i=0; i<obj.records.length; i++) {
+                        let data = obj.records[i];
+
+                        Object.keys(data).forEach(function(key, index, ar) {
+                            if ((typeof data[key]) !== 'object') {
+                                let val = data[key];
+                                if (mergeFields[sfdc_childobj][key]) {
+                                    let el = mergeFields[sfdc_childobj][key].el;
+                                    if (el) {
+                                        //差し込みフィールドの削除、およびテキスト挿入
+                                        el.element(openXml.W.fldSimple).remove();
+                                        el.add(new Ltxml.XElement(openXml.W.r, new Ltxml.XElement(openXml.W.t, val)));
+                                    }
+                                }
+                            }
+                        });
+                        break;//とりあえず
+                    }
+                }
+            }
+        });
     };
 
     openXml.Word.prototype.save = function(reportName) {
@@ -37,7 +81,7 @@
                 let obj = {}
                 obj.el = p;
                 obj.fieldName = fld.element(openXml.W.r).element(openXml.W.t).value;
-                obj.sfdcInfo = obj.fieldName.match(/[A-Za-z]+/g);
+                obj.sfdcInfo = obj.fieldName.match(/[A-Za-z\.]+/g);
 
                 let sfdc_colname = obj.sfdcInfo[1];
                 mergeFields[sfdc_colname] = obj;
@@ -71,6 +115,7 @@
                             }
 
                             let sfdc_colname = obj.sfdcInfo[2];
+                            mergeFields[sfdc_childobj].el = tr;
                             mergeFields[sfdc_childobj][sfdc_colname] = obj;
                         }
                     }
