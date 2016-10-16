2583ff4058292743a18aa1bf27f97a07f874d33c
xkonishi <konishi.cnb@gmail.com>
2016-10-17 00:31:07 +0900

    n

--- openxml-word.js
+++ openxml-word.js
@@ -12,6 +12,60 @@
     };
 
     openXml.Word.prototype.merge = function(mergedata) {
+        let data = mergedata[0];
+        let body = mnXDoc.root.element(openXml.W.body);
+
+        let flds = openXml.Util.findElements(body, openXml.W.fldSimple, openXml.W.tbl);
+        flds.forEach(function(fld, index, ar) {
+            let fieldName = fld.element(openXml.W.r).element(openXml.W.t).value;
+            let sobjInfo = fieldName.match(/[A-Za-z\.]+/g);
+
+            if (sobjInfo.length === 2) {
+                let colname = sobjInfo[1];
+
+                if (data[colname]) {
+                    let val = data[colname];
+                    if (val) {
+                        //テキスト挿入、および差し込みフィールドの削除
+                        fld.parent.add(new Ltxml.XElement(openXml.W.r, new Ltxml.XElement(openXml.W.t, val)));
+                        fld.remove();
+                    }
+                }
+            }
+        });
+
+        let tbls = body.elements(openXml.W.tbl);
+        tbls.forEach(function(tbl, index, ar) {
+            let exeptElements = [openXml.W.tblPr, openXml.W.tblGrid];
+            let flds = openXml.Util.findElements(tbl, openXml.W.fldSimple, exeptElements);
+
+            //１行目のデータ設定
+            flds.forEach(function(fld, index, ar) {
+                let fieldName = fld.element(openXml.W.r).element(openXml.W.t).value;
+                let sobjInfo = fieldName.match(/[A-Za-z\.]+/g);
+
+                if (sobjInfo.length === 3) {
+                    let objname = sobjInfo[1];
+                    let colname = sobjInfo[2];
+
+                    if (data[objname] && data[objname].records.length > 0) {
+                        let val = data[objname].records[0][colname];
+                        if (val) {
+                            //テキスト挿入、および差し込みフィールドの削除
+                            fld.parent.add(new Ltxml.XElement(openXml.W.r, new Ltxml.XElement(openXml.W.t, val)));
+                            fld.remove();
+                        }
+                    }
+                }
+            });
+
+            //２行目以降を追加
+        });
+
+
+
+
+        /*
         let mergeFields = _getMergeFields();
 
         let data = mergedata[0];
@@ -65,7 +119,7 @@
                     }
                 }
             }
-        });
+        });*/
     };
 
     openXml.Word.prototype.save = function(reportName) {
