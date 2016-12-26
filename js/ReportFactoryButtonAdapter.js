62895a95083893c8cd62f72f49cf8e7a820b1144
xkonishi <konishi.cnb@gmail.com>
2016-12-26 17:35:03 +0900

    c

--- js/ReportFactoryButtonAdapter.js
+++ js/ReportFactoryButtonAdapter.js
@@ -1,9 +1,14 @@
-function doDownload(sessionId, templateId, reportName, recordId) {
+function doDownload(sessionId, templateUrl, reportName, recordId) {
 	console.log('[doDownload] sessionId=' + sessionId);
 	console.log('[doDownload] templateId=' + templateId);
 	console.log('[doDownload] reportName=' + reportName);
 	console.log('[doDownload] recordId=' + recordId);
 	
+	let index = templateUrl.lastIndexOf('/');
+	let templateId = templateUrl.substring(index+1);
+	let bseUrl = templateUrl.substring(0, index);
+	
+	ReportFactory.init(sessionId, bseUrl);
 	ReportFactory.create(
 		sessionId,
 		templateId,
